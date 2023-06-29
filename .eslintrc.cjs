42f30cd7b5fd73cad431702cb9ea0a6c30a31139
Barbara <barbara.taskinen@gmail.com>
2023-06-29 16:09:20 +0300

    add eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,15 @@ module.exports = {
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', 'always'],
+    'no-console': 0,
     'react-refresh/only-export-components': 'warn',
   },
-}
+};
