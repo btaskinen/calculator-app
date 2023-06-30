d941d613479eacf0728cf4fb1f6d7f23b48b05c4
Barbara <barbara.taskinen@gmail.com>
2023-06-30 13:49:28 +0300

    create functions for calculations
    - property type was added to ButtonData to distinguish between the actions a button can execute
    - functions for the different button types were create
    - functionalities do not yet quite work

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { useState } from 'react';
 import Button from './Button';
 import './App.css';
 
+export type ButtonType = 'ACTION' | 'OPERATOR' | 'NUMBER';
+
 export type ButtonData = {
   label: string;
+  type: ButtonType;
   color: string;
 };
 
@@ -12,91 +15,178 @@ type Buttons = ButtonData[];
 const buttons: Buttons = [
   {
     label: 'AC',
+    type: 'ACTION',
     color: 'darker',
   },
   {
     label: '+/-',
+    type: 'ACTION',
     color: 'darker',
   },
   {
     label: '%',
+    type: 'ACTION',
     color: 'darker',
   },
   {
     label: '/',
+    type: 'OPERATOR',
     color: 'dark',
   },
   {
     label: '7',
+    type: 'NUMBER',
     color: 'light',
   },
   {
     label: '8',
+    type: 'NUMBER',
     color: 'light',
   },
   {
     label: '9',
+    type: 'NUMBER',
     color: 'light',
   },
   {
     label: 'X',
+    type: 'OPERATOR',
     color: 'dark',
   },
   {
     label: '4',
+    type: 'NUMBER',
     color: 'light',
   },
   {
     label: '5',
+    type: 'NUMBER',
     color: 'light',
   },
   {
     label: '6',
+    type: 'NUMBER',
     color: 'light',
   },
   {
     label: '-',
+    type: 'OPERATOR',
     color: 'dark',
   },
   {
     label: '1',
+    type: 'NUMBER',
     color: 'light',
   },
   {
     label: '2',
+    type: 'NUMBER',
     color: 'light',
   },
   {
     label: '3',
+    type: 'NUMBER',
     color: 'light',
   },
   {
     label: '+',
+    type: 'NUMBER',
     color: 'dark',
   },
   {
     label: '0',
+    type: 'NUMBER',
     color: 'light',
   },
   {
     label: '.',
+    type: 'NUMBER',
     color: 'light',
   },
   {
     label: '=',
+    type: 'OPERATOR',
     color: 'dark',
   },
 ];
 
 const App = () => {
+  const [displayNumber, setDisplayNumber] = useState(0);
   const [calcNumber, setCalcNumber] = useState(0);
+  const [operator, setOperator] = useState('');
+
+  const operatorButtonPressed = (label: string): void => setOperator(label);
+
+  const numberButtonPressed = (label: string): void => {
+    switch (operator) {
+      case '':
+        if (displayNumber === 0) {
+          setDisplayNumber(Number(label));
+          setCalcNumber(Number(label));
+        } else {
+          const numberString = calcNumber + label;
+          setCalcNumber(Number(numberString));
+          setDisplayNumber(Number(numberString));
+        }
+        break;
+      case '+':
+        setCalcNumber(calcNumber + Number(label));
+        break;
+      case '-':
+        setCalcNumber(calcNumber - Number(label));
+        break;
+      case 'X':
+        setCalcNumber(calcNumber * Number(label));
+        break;
+      case '/':
+        setCalcNumber(calcNumber / Number(label));
+        break;
+      case '=':
+        setDisplayNumber(calcNumber);
+    }
+    setOperator('');
+  };
+
+  const actionButtonPressed = (label: string): void => {
+    switch (label) {
+      case 'AC':
+        setCalcNumber(0);
+        setDisplayNumber(0);
+        break;
+      case '+/-':
+        setCalcNumber(calcNumber * -1);
+        setDisplayNumber(displayNumber * -1);
+        break;
+      case '%':
+        setCalcNumber(calcNumber / 100);
+        setDisplayNumber(displayNumber / 100);
+        break;
+    }
+  };
+
+  const actionSelection = (type: ButtonType, label: string): void => {
+    switch (type) {
+      case 'ACTION':
+        actionButtonPressed(label);
+        break;
+      case 'NUMBER':
+        numberButtonPressed(label);
+        break;
+      case 'OPERATOR':
+        operatorButtonPressed(label);
+    }
+  };
 
   return (
     <div className="App">
-      <div className="App_text-box">{calcNumber}</div>
+      <div className="App_text-box">{displayNumber}</div>
       <div className="App_buttons">
         {buttons.map((button, index) => (
-          <Button key={index} button={button} />
+          <Button
+            key={index}
+            button={button}
+            buttonFunction={actionSelection}
+          />
         ))}
       </div>
     </div>
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,13 +1,18 @@
 import './Buttons.css';
 import { ButtonData } from './App';
+import { ButtonType } from './App';
 
 type Props = {
   button: ButtonData;
+  buttonFunction: (type: ButtonType, label: string) => void;
 };
 
-const Button = ({ button }: Props) => {
+const Button = ({ button, buttonFunction }: Props) => {
   return (
-    <button className={`Button_button Button_color-${button.color}`}>
+    <button
+      className={`Button_button Button_color-${button.color}`}
+      onClick={() => buttonFunction(button.type, button.label)}
+    >
       {button.label}
     </button>
   );
